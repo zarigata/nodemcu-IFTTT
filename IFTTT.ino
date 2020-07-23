877cf89822c1a4ed6ea4d79a7f3558c85bb65321
zarigata <39637452+zarigata@users.noreply.github.com>
2020-07-23 01:13:14 -0300

    Add files via upload

---
+++ IFTTT.ino
@@ -0,0 +1,81 @@
+#include "IFTTTWebhook.h"
+#include <ESP8266WiFi.h>
+//----------------------
+#define ssid "ATHENA"
+#define password "Hercules12"
+#define IFTTT_API_KEY ""
+#define IFTTT_EVENT_NAME "delorean1"
+//-----------------------------
+//ARDUINO ID
+int cl = 5;
+int sent = 14;
+int button = 4;
+int buttonState = 0;
+//-----------------------------
+void setup() {
+  Serial.begin(115200);
+ // while (!Serial) {
+ // }
+  Serial.println(" ");
+  Serial.println(" ");
+  connectToWifi();
+  pinMode (cl , OUTPUT);
+  pinMode (button , INPUT);
+  pinMode (sent , OUTPUT);
+  //ESP.deepSleep(wakePin);
+
+}
+
+
+
+void connectToWifi() {
+  digitalWrite(cl, LOW);
+  Serial.print("Connecting to: "); //uncomment next line to show SSID name
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  Serial.println(" ");// print an empty line
+  Serial.print("Attempting to connect: ");
+
+  //try to connect for 10 seconds
+  int i = 20;
+  while (WiFi.status() != WL_CONNECTED && i >= 0) {
+    delay(700);
+    Serial.print(i);
+    Serial.print(", ");
+    i--;
+  }
+  Serial.println(" ");// print an empty line
+
+  //print connection result
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.print("Connected.");
+    Serial.println(" ");// print an empty line
+    Serial.print("NodeMCU ip address: ");
+    Serial.println(WiFi.localIP());
+    analogWrite(cl , 15);
+  }
+  else {
+    Serial.println("Connection failed - check your credentials or connection");
+    delay(60000);
+    ESP.restart();
+  }
+}
+void loop()
+{
+  buttonState = digitalRead(button);
+  if (buttonState == 1)
+  {
+    analogWrite(sent , 50);
+    IFTTTWebhook hook(IFTTT_API_KEY, IFTTT_EVENT_NAME);
+   // hook.trigger();
+    Serial.println("SENT");
+    delay (5000);
+
+  }
+  if (buttonState == 0)
+  {
+    analogWrite(sent , LOW);
+    Serial.println("NOT   SENT");
+    delay (100);
+  }
+}
